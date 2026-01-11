40ef0f11cd383cf74c230464f4a10e7c1cfac181
chief-micco <github@kcwolf.net>
2026-01-11 07:47:59 -0700

    add placeholder for metastore compatibility tests

---
+++ csharp/AppEncryption/AppEncryption.Tests/AppEncryption/PlugIns/Aws/Metastore/DynamoDbMetastoreCompatibilityTests.cs
@@ -0,0 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace GoDaddy.Asherah.AppEncryption.Tests.AppEncryption.PlugIns.Aws.Metastore;
+
+[ExcludeFromCodeCoverage]
+public class DynamoDbMetastoreCompatibilityTests
+{
+}
