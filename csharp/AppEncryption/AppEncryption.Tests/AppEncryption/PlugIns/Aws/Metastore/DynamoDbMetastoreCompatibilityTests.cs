7e1a4bdc07e41898e917d5417b2a90765ed493a7
chief-micco <github@kcwolf.net>
2026-01-12 07:19:08 -0700

    compatibility tests already exist

--- csharp/AppEncryption/AppEncryption.Tests/AppEncryption/PlugIns/Aws/Metastore/DynamoDbMetastoreCompatibilityTests.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace GoDaddy.Asherah.AppEncryption.Tests.AppEncryption.PlugIns.Aws.Metastore;
-
-[ExcludeFromCodeCoverage]
-public class DynamoDbMetastoreCompatibilityTests
-{
-}
